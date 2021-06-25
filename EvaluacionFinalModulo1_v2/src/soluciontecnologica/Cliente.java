5b8660db16d2976f08b51f758f1333d3d1f9ce8f
Burzumhell <ebae82@hotmail.com>
2021-06-24 22:16:05 -0400

    Update Cliente.java

--- EvaluacionFinalModulo1_v2/src/soluciontecnologica/Cliente.java
+++ EvaluacionFinalModulo1_v2/src/soluciontecnologica/Cliente.java
@@ -1,163 +1,150 @@
 package soluciontecnologica;
-import java.util.Scanner;
 /**
- * Clase para el registro de visita en terreno
+ * Clase para el registro de usuario perfil Cliente
  * @author The Bubble
  * @version 1.0
  */
-public class VisitaEnTerreno {
+public class Cliente extends Usuario {
 	/**
 	 * Atributos que definen la clase
 	 */
-	private int id; // Obligatorio
-	private String rutCliente; // Obligatorio
-	private String dia;
-	private int hora; // HH
-	private int minutos; // MM
-	private String lugar; // Obligatorio
-	private String comentarios;
+	private int rut;
+	private String direccion;
+	private String comuna;
+	private int cantidadEmpleados;
+	private String telefono;
+	private String razonSocial;
 	
 	/**
 	 * Constructor vacío o por defecto
 	 */
-	public VisitaEnTerreno() {
+	public Cliente() {
 		
 	}
 
 	/**
-	 * Constructor con entrada de datos
-	 * @param id de la visita en terreno
-	 * @param rutCliente de la visita en terreno
-	 * @param dia de la visita en terreno
-	 * @param hora de la visita en terreno
-	 * @param lugar de la visita en terreno
-	 * @param comentarios de la visita en terreno
+	 * Constructor con datos de entrada
+	 * @param rut del cliente
+	 * @param direccion del cliente
+	 * @param comuna del cliente
+	 * @param cantidadEmpleados del cliente
+	 * @param telefono del cliente
+	 * @param razonSocial del cliente
 	 */
-	public VisitaEnTerreno(int id, String rutCliente, String dia, int hora,
-			int minutos, String lugar, String comentarios) {
+	public Cliente(int rut, String direccion, String comuna,
+			int cantidadEmpleados, String telefono, String razonSocial) {
 		super();
-		this.id = id;
-		this.rutCliente = rutCliente;
-		this.dia = dia;
-		this.hora = hora;
-		this.minutos = minutos;
-		this.lugar = lugar;
-		this.comentarios = comentarios;
+		this.rut = rut;
+		this.direccion = direccion;
+		this.comuna = comuna;
+		this.cantidadEmpleados = cantidadEmpleados;
+		this.telefono = telefono;
+		this.razonSocial = razonSocial;
 	}
 
 	// Getters and Setters
-	public int getId() {
-		return id;
+	public int getRut() {
+		return rut;
 	}
 
-	public String getRutCliente() {
-		return rutCliente;
+	public String getDireccion() {
+		return direccion;
 	}
 
-	public String getDia() {
-		return dia;
+	public String getComuna() {
+		return comuna;
 	}
 
-	public int getHora() {
-		return hora;
+	public int getCantidadEmpleados() {
+		return cantidadEmpleados;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public String getRazonSocial() {
+		return razonSocial;
+	}
+
+	public void setRut(int rut) {
+		this.rut = rut;
+		while (this.rut > 99999999) {
+			this.rut = leerInt("Ingrese un run válido por favor");
+		}
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+		while (this.direccion.length() > 70) {
+			this.direccion = leerString("Ingrese máximo 70 caracteres");
+		}
+	}
+
+	public void setComuna(String comuna) {
+		this.comuna = comuna;
+		while (this.comuna.length() > 50) {
+			this.comuna = leerString("Ingrese máximo 50 caracteres");
+		}
+	}
+
+	public void setCantidadEmpleados(int cantidadEmpleados) {
+		this.cantidadEmpleados = cantidadEmpleados;
+		while (this.cantidadEmpleados < 1) {
+			this.cantidadEmpleados = leerInt("Ingresa un valor mayor a cero");
+		}
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+		while (this.telefono.isEmpty() || this.telefono.isBlank()) {
+			this.telefono = leerString("Este campo es obligatorio");
+		}
+	}
+
+	public void setRazonSocial(String razonSocial) {
+		this.razonSocial = razonSocial;
+		while (this.razonSocial.length() > 50) {
+			this.razonSocial = leerString("Ingrese máximo 50 caracteres");
+		}
+	}
+
+	/**
+	 * Método obtenerNombre
+	 * @return nombre de la razón social del cliente
+	 */
+	public String obtenerNombre() {
+		return razonSocial;
 	}
 	
-	public int getMinutos() {
-		return minutos;
-	}
-
-	public String getLugar() {
-		return lugar;
-	}
-
-	public String getComentarios() {
-		return comentarios;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public void setRutCliente(String rutCliente) {
-		this.rutCliente = rutCliente;
-		while (this.rutCliente.isEmpty() || this.rutCliente.isBlank()) {
-			this.rutCliente = leerString("Ingrese un rut válido");
-		}
-	}
-
-	public void setDia(String dia) {
-		this.dia = dia;
-	}
-
-	public void setHora(int hora) {
-		this.hora = hora;
-		while (this.hora > 23) {
-			this.hora = leerInt("Ingrese un valor de 0 a 23");
-		}
+	/**
+	 * Método obtenerNombreUsuario
+	 * @return nombre del usuario
+	 */
+	public String obtenerNombreUsuario() {
+		return this.getNombre();
 	}
 	
-	public void setMinutos(int minutos) {
-		this.minutos = minutos;
-		while (this.minutos > 59) {
-			this.minutos = leerInt("Ingrese un valor de 0 a 59");			
-		}
-	}
-
-	public void setLugar(String lugar) {
-		this.lugar = lugar;
-		while (this.lugar.isEmpty() || this.lugar.isBlank()
-			|| this.lugar.length() < 10 || this.lugar.length() > 50) {
-			this.lugar = leerString("Este campo es obligatorio, mínimo 10 y"
-					+ " máximo 50 caracteres");
-		}
-	}
-
-	public void setComentarios(String comentarios) {
-		this.comentarios = comentarios;
-		while (this.comentarios.length() > 100) {
-			this.comentarios = leerString("Ingrese máximo 100 caracteres");
-		}
-	}
-
 	/**
 	 * Método toString
 	 */
 	@Override
 	public String toString() {
-		return "VisitaEnTerreno [id=" + id + ", rutCliente=" + rutCliente
-			+ ", dia=" + dia + ", hora=" + hora + ", minutos=" + minutos
-			+ ", lugar=" + lugar + ", comentarios=" + comentarios + "]";
+		return "\nUsuario\nNombre: " + super.getNombre()
+				+ "\nFecha de nacimiento: " + super.getFechaNacimiento()
+				+ "\nRUN: " + super.getRun() + "\nTipo de usuario: "
+				+ super.getTipoUsuario() + "\n[Rut: " + rut + ", Dirección: "
+				+ direccion + ", Comuna: " + comuna + ", Cantidad de empleados: "
+				+ cantidadEmpleados + ", Telefono: " + telefono
+				+ ", RazonSocial: " + razonSocial + "]";
 	}
-	
+
 	/**
-	 * Método para lectura datos tipo String
-	 * @param mensaje que recibe para ser mostrado
-	 * @return dato ingresado por el usuario
+	 * Método analizarUsuario
 	 */
-	public static String leerString(String mensaje) {
-		escribir(mensaje);
-		Scanner entrada = new Scanner(System.in);
-		String dato = entrada.nextLine();
-		return dato;
-	}
-	
-	/**
-	 * Método para lectura datos tipo int
-	 * @param mensaje que recibe para ser mostrado
-	 * @return dato ingresado por el usuario
-	 */
-	public static int leerInt(String mensaje) {
-		escribir(mensaje);
-		Scanner entrada = new Scanner(System.in);
-		int dato = entrada.nextInt();
-		return dato;
-	}
-	
-	/**
-	 * Método que muestra mensaje en pantalla
-	 * @param mensaje que será mostrado
-	 */
-	public static void escribir(String mensaje) {
-		System.out.println(mensaje);
+	@Override
+	public void analizarUsuario() {
+		super.analizarUsuario();
+		System.out.println("Dirección: " + direccion + ", Comuna: " + comuna);
 	}
 }
